1f048d3553ce134202db74b60b58163b318b8f76
ZacharySabourin <zachary.sabourin@gmail.com>
2021-07-09 17:26:17 -0400

    restructuring

--- BackEnd/dao/movies.dao.js
+++ BackEnd/dao/movies.dao.js
@@ -1,6 +1,6 @@
 import mongodb from 'mongodb'
-const ObjectID = mongodb.ObjectID;
 
+const ObjectID = mongodb.ObjectID;
 let movies;
 
 export default class MoviesDAO
@@ -20,49 +20,76 @@ export default class MoviesDAO
         }
     }
 
-    static async getMovies({
-        filters = null,
-        page = 0,
-        moviesPerPage = 20,
-    } = {})
-    {
-        console.log(movies);
-        let query;
-
-        if(filters) 
-        {
-            if ('title' in filters)
-                query = { $test: { $search: filters.title }};
-            else if ('year' in filters)
-                query = { 'year': { $eq: filters.year }};
-        }
-
-        let cursor;
-
-        try
-        {
-            cursor = await movies.find(query);
-        }
-        catch(e)
-        {
-            console.error('Unable to issue find command, ' + e);
-            return { movieList: [], totalNumMovies: 0 };
-        }
-
+    static async getMovies({ filters = null, page = 0, moviesPerPage = 20 } = {})
+    {      
+        const query = buildMovieQuery(filters);        
+        const cursor = buildMovieCursor(query);        
         const displayCursor = cursor.limit(moviesPerPage).skip(moviesPerPage * page);
-
-        try 
-        {
-            //Get all restaurants and count the documents
-            const movieList = displayCursor.toArray();
-            const totalNumMovies = movies.countDocuments(query);
-
-            return { movieList, totalNumMovies };
-        } 
-        catch (e) 
-        {
-            console.error('Unable to convert cursor to array or problem counting documents, ' + e);
-            return { movieList: [], totalNumMovies: 0 };
-        }
+        
+        return { movieList: buildMovieList(displayCursor), totalNumMovies: getNumMovies(query) };
     }
 }
+
+const getNumMovies = query => {
+
+    const totalNumMovies = 0;
+
+    try 
+    {
+        totalNumMovies = movies.countDocuments(query);
+    } 
+    catch (e) 
+    {
+        console.error('Problem counting documents, ' + e);      
+    }
+
+    return totalNumMovies;
+};
+
+const buildMovieList = displayCursor => {
+
+    const movieList = [];
+
+    try
+    {
+        movieList = displayCursor.toArray();
+    }
+    catch(e)
+    {
+        console.error('Unable to convert cursor to array, ' + e);
+    }
+
+    return movieList = displayCursor.toArray();
+};
+
+
+const buildMovieQuery = filters => {
+    let query = {};
+
+    if(filters) 
+    {
+        if ('title' in filters)
+            query = { $text: { $search: filters.title }};
+        else if ('year' in filters)
+            query = { 'year': { $eq: filters.year }};
+    }
+
+    return query;
+};
+
+const buildMovieCursor = query => {
+    
+    let cursor = {};
+
+    try
+    {
+        cursor = await movies.find(query);
+    }
+    catch(e)
+    {
+        console.error('Unable to issue find command, ' + e);
+        cursor = { movieList: [], totalNumMovies: 0 };
+    }
+
+    return cursor;
+};
